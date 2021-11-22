47400005f4dc4359c895c7e13545ee622fef72c3
supermancash <emil@triest.de>
2021-11-22 19:30:37 +0100

    removing db password

--- backend/dao/mongoConnection.js
+++ backend/dao/mongoConnection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
+const config = require("../config/config");
 
 
 const mongoConnection = () => {
-    const uri = "mongodb+srv://supermancash:x.psVy33eer7T%40Y@pushnotifications.erof5.mongodb.net/uniecommerce_db?retryWrites=true&w=majority";
-    mongoose.connect(uri,
+    mongoose.connect(config.mongoUri,
         {useNewUrlParser: true, useUnifiedTopology: true},
         err => {console.log(err)}
     );
